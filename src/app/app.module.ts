e0a96dcdad3ef2ecbf70847c3a3027e85bc6912c
Thomas Blaisot <t.blaisot@groupeonepoint.com>
2021-04-01 14:26:24 +0200

    fix angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
+import { LionWcChoiceValueAccessorDirective } from "./choicevalue.directive";
+import { LionWcModelValueAccessorDirective } from "./modelvalue.directive";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    LionWcChoiceValueAccessorDirective,
+    LionWcModelValueAccessorDirective
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule { }
